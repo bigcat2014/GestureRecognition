b9afbf5cc737b540f90bffe23630cb7f1a6662b7
bigcat2014 <ltogan.2014@gmail.com>
2018-07-15 23:04:07 -0400

    AlexaPi network trigger test

---
+++ src/main.py
@@ -0,0 +1,30 @@
+import select
+import socket
+import gesture_recognition.config
+import yaml
+import json
+
+# config = None
+# config_exists = gesture_recognition.config.filename is not None
+#
+# if config_exists:
+# 	with open(gesture_recognition.config.filename, 'r') as stream:
+# 		config = yaml.load(stream)
+#
+# if config_exists:
+# 	port = config.get('logging', 'INFO').upper()
+
+# Create the client.
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('10.0.0.254', 328))
+
+msg = {}
+msg_header = {'type': 'trigger'}
+msg_body = {}
+
+msg['message_header'] = msg_header
+msg['message_body'] = msg_body
+
+if __name__ == '__main__':
+	client_socket.send(json.dumps(msg))
+	client_socket.close()
